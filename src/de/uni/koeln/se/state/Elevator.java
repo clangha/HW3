6b890ddc69eb546f9342d9fcf8e646d18854cf0d
clangha <clangha2@smail.uni-koeln.de>
2021-11-01 13:30:39 +0100

    First commit for HW3 - Task 3

---
+++ src/de/uni/koeln/se/state/Elevator.java
@@ -0,0 +1,53 @@
+package de.uni.koeln.se.state;
+
+
+public class Elevator{
+
+    public static int current_Floor=5;
+    int dest_Floor;
+
+    private Elv_States State = new Elv_States();
+
+    public Elevator (int dest_floor)
+    {
+        this.dest_Floor=dest_floor;
+        System.out.println("*** Destination floor is: "+ dest_Floor);
+
+        State.Current_State = State.Idle;
+
+        if (current_Floor < dest_Floor){
+            go_up();
+        }else if (current_Floor > dest_Floor){
+            go_down();
+        }else if (current_Floor == dest_Floor){
+            arrive_atFloor();
+        }
+
+
+    }
+
+    private void arrive_atFloor() {
+        System.out.println("*** Arrived at floor "+current_Floor);
+        State.Current_State = State.Idle;
+    }
+
+    private void go_up(){
+        State.Current_State = State.Moving_up;
+        current_Floor = current_Floor+1;
+        System.out.println(State.Current_State+" heading "+current_Floor);
+        if (current_Floor == dest_Floor){
+            arrive_atFloor();
+        }else go_up();
+
+    }
+
+    private void go_down(){
+        State.Current_State = State.Moving_down;
+        current_Floor = current_Floor-1;
+        System.out.println(State.Current_State+" heading "+current_Floor);
+        if (current_Floor == dest_Floor){
+            arrive_atFloor();
+        }else go_down();
+    }
+
+}
